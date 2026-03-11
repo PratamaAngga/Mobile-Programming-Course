a067dcb19ea64092f3b3e2d84dab24457b2b641f
PratamaAngga <anggashillnoir@gmail.com>
2026-03-11 09:44:05 +0700

    Persiapan pengerjaan: Pembuatan template project dart

---
+++ Quiz1/Src/kuis_244107060113/bin/kuis_244107060113.dart
@@ -0,0 +1,5 @@
+import 'package:kuis_244107060113/kuis_244107060113.dart' as kuis_244107060113;
+
+void main(List<String> arguments) {
+  print('Hello world: ${kuis_244107060113.calculate()}!');
+}
---
+++ Quiz1/Src/kuis_244107060113/test/kuis_244107060113_test.dart
@@ -0,0 +1,8 @@
+import 'package:kuis_244107060113/kuis_244107060113.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
